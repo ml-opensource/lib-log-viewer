885faed673a03348d3a47277940c2d51749502b6
Matko Smoljan <mats@nodesagency.com>
2018-12-04 13:56:52 +0100

    Update tests

--- logviewer/src/androidTest/java/com/nodesagency/logviewer/LoggerTest.kt
+++ logviewer/src/androidTest/java/com/nodesagency/logviewer/LoggerTest.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import com.nodesagency.logviewer.data.database.DatabaseLogRepository
 import com.nodesagency.logviewer.data.database.LogDatabase
 import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -28,11 +29,12 @@ class LoggerTest {
     }
 
     @Test
-    fun logs_with_general_category_name_if_one_is_not_provided() {
+    fun logs_with_general_category_name_if_one_is_not_provided() = runBlocking {
         val message = "A message"
-        Logger.log(message)
+        Logger.log(message).join() // log() is run asynchronously, so we use join() to wait for it to finish
 
-        val categories = logRepository.getAllCategoriesAlphabeticallySorted()
+        val id = logRepository.getIdForCategoryName("General") ?: throw IllegalStateException("General ID doesn't exist")
+        val categories = logRepository.getLogEntriesForCategory(id)
         val generalLogEntries = logRepository.getLogEntriesForCategory(categoryId = GENERAL_CATEGORY_ID)
 
         assertEquals(1, categories.size)
