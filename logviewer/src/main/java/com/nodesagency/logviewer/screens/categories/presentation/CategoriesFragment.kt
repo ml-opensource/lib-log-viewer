3a486e4a41c9e213ced69e97447236f5d5090084
Matko Smoljan <mats@nodesagency.com>
2018-12-04 13:44:49 +0100

    Add divider between list items

--- logviewer/src/main/java/com/nodesagency/logviewer/screens/categories/presentation/CategoriesFragment.kt
+++ logviewer/src/main/java/com/nodesagency/logviewer/screens/categories/presentation/CategoriesFragment.kt
@@ -7,10 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.nodesagency.logviewer.Logger
 import com.nodesagency.logviewer.R
+
 
 class CategoriesFragment : Fragment() {
 
@@ -37,11 +40,14 @@ class CategoriesFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = LayoutInflater.from(context).inflate(R.layout.fragment_categories, container, false)
+        val view = inflater.inflate(R.layout.fragment_categories, container, false)
 
         categoriesRecyclerView = view.findViewById<RecyclerView>(R.id.categoriesRecyclerView)
+        val dividerDecoration = DividerItemDecoration(inflater.context, VERTICAL)
+
         categoriesRecyclerView.adapter = categoriesAdapter
         categoriesRecyclerView.layoutManager = layoutManager
+        categoriesRecyclerView.addItemDecoration(dividerDecoration)
 
         return view
     }
