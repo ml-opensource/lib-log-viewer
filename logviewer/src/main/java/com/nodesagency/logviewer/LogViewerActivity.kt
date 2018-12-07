0bfb0501a8df999aa65fd5eb149382a00ff6c137
Matko Smoljan <mats@nodesagency.com>
2018-12-07 14:32:47 +0100

    Update title with fragment change

--- logviewer/src/main/java/com/nodesagency/logviewer/LogViewerActivity.kt
+++ logviewer/src/main/java/com/nodesagency/logviewer/LogViewerActivity.kt
@@ -4,15 +4,17 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import com.nodesagency.logviewer.data.model.Category
 import com.nodesagency.logviewer.screens.categories.CategoriesFragment
 import com.nodesagency.logviewer.screens.categories.OnCategorySelectListener
 import com.nodesagency.logviewer.screens.log.LogFragment
 
 private const val FRAGMENT_CATEGORIES = "categories"
-private const val FRAGMENT_LOG = "log"
+private const val FRAGMENT_LOGS = "logs"
+private const val STATE_TITLE = "state_title"
 
-class LogViewerActivity : AppCompatActivity(), OnCategorySelectListener {
+class LogViewerActivity : AppCompatActivity(), OnCategorySelectListener, FragmentManager.OnBackStackChangedListener {
 
     companion object {
         fun createIntent(context: Context) = Intent(context, LogViewerActivity::class.java)
@@ -22,11 +24,34 @@ class LogViewerActivity : AppCompatActivity(), OnCategorySelectListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_viewer)
 
-        title = getString(R.string.title_log_categories)
-
         if (savedInstanceState == null) {
             showCategoriesFragment()
         }
+
+        title = savedInstanceState?.getString(STATE_TITLE) ?: getString(R.string.title_log_categories)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+
+        if (title != null && title.isNotBlank()) {
+            outState.putString(STATE_TITLE, title.toString())
+        } else {
+            outState.remove(STATE_TITLE)
+        }
+
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        supportFragmentManager.addOnBackStackChangedListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        supportFragmentManager.removeOnBackStackChangedListener(this)
     }
 
     private fun showCategoriesFragment() {
@@ -37,6 +62,8 @@ class LogViewerActivity : AppCompatActivity(), OnCategorySelectListener {
     }
 
     override fun onCategorySelected(category: Category) {
+        title = category.name
+
         supportFragmentManager
             .beginTransaction()
             .apply {
@@ -45,5 +72,14 @@ class LogViewerActivity : AppCompatActivity(), OnCategorySelectListener {
             .addToBackStack(null)
             .add(R.id.fragmentContainerView, LogFragment.newInstance(category))
             .commit()
+    }
+
+    override fun onBackStackChanged() {
+        val categoriesFragment = supportFragmentManager.findFragmentByTag(FRAGMENT_CATEGORIES)
+        val areCategoriesShown = categoriesFragment != null && !categoriesFragment.isHidden
+
+        if (areCategoriesShown) {
+            title = getString(R.string.title_log_categories)
+        }
     }
 }
