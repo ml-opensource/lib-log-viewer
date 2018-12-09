48c11fa326e8b61350129d4e41f92e16e581be03
Matko Smoljan <mats@nodesagency.com>
2018-12-09 16:22:26 +0100

    Rename LogFragment to LogsFragment

--- logviewer/src/main/java/com/nodesagency/logviewer/screens/logs/LogsFragment.kt
+++ logviewer/src/main/java/com/nodesagency/logviewer/screens/logs/LogsFragment.kt
@@ -51,7 +51,7 @@ internal class LogFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_log, container, false)
+        val view = inflater.inflate(R.layout.fragment_logs, container, false)
         val resources = inflater.context.resources
 
         logEntriesRecyclerView = view.findViewById(R.id.logEntriesView)
